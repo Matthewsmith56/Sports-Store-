1b9a883a55e2cb05b78f372f64f3c8fd7414c53c
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-23 08:38:47 -0400

    Completed through Page 200

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -30,7 +30,8 @@ namespace SportsStore.UnitTests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Act
-            ProductsListViewModel result = (ProductsListViewModel) controller.List(2).Model;
+            ProductsListViewModel result 
+                = (ProductsListViewModel)controller.List(null, 2).Model;
             // Assert
             Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
@@ -76,7 +77,8 @@ namespace SportsStore.UnitTests
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
             // Act
-            ProductsListViewModel result = (ProductsListViewModel)controller.List(2).Model;
+            ProductsListViewModel result 
+                = (ProductsListViewModel)controller.List(null, 2).Model;
             // Assert
             PagingInfo pageInfo = result.PagingInfo;
             Assert.AreEqual(pageInfo.CurrentPage, 2);
