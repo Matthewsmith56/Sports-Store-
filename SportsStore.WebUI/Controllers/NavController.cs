9bff90f74b3302b84f0f8dafdf9fe10d5c36ad9e
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-23 11:40:36 -0400

    Completed through page 208

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -1,16 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using System.Linq;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class NavController : Controller
     {
-        public string Menu()
+        private IProductRepository repository;
+
+        public NavController(IProductRepository repo)
         {
-            return "Hello from NavController";
+            repository = repo;
+        }
+
+        public PartialViewResult Menu()
+        {
+            IEnumerable<string> categories = repository.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x);
+            return PartialView(categories);
         }
     }
 }
